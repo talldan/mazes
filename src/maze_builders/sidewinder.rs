d3b3a0b39349156ab7304f05d1e8061b11814791
Daniel Richards <daniel.richards@automattic.com>
2025-03-27 18:08:38 +0800

    Simplify sidewinder

--- src/maze_builders/sidewinder.rs
+++ src/maze_builders/sidewinder.rs
@@ -13,9 +13,10 @@ pub fn carve_sidewinder_into_grid_map(grid_map: &GridMap) -> HashSet<Wall> {
         let coinflip = fastrand::bool();
         let mut direction = get_direction_from_coinflip(coinflip);
 
-        // Check whether we're on the northern most row, and if so, flip direction.
-        let neighbour = grid_map.neighbour_from_cell_pos(cell_pos, direction);
-        if neighbour.is_none() {
+        let mut wall = grid_map.inner_wall_from_cell_pos(cell_pos, direction);
+
+        // If we've selected a wall at the boundary then flip the direction.
+        if wall.is_none() {
             direction = get_direction_from_coinflip(!coinflip);
         }
 
@@ -23,32 +24,21 @@ pub fn carve_sidewinder_into_grid_map(grid_map: &GridMap) -> HashSet<Wall> {
         // 'run' of cells until we hit the boundary, at which point we switch direction
         // to north.
         if direction == Dir2::EAST {
-            let neighbour = grid_map.neighbour_from_cell_pos(cell_pos, direction);
-
-            // Switch direction if there's no neighbour in that direction.
-            // This avoids carving through the outer walls.
-            if neighbour.is_some() {
-                let wall = grid_map.wall_from_cell_pos(cell_pos, direction);
-                if let Some(wall) = wall {
-                    removed_walls.insert(wall);
-                }
-            }
+            wall = grid_map.inner_wall_from_cell_pos(cell_pos, direction);
         }
 
-        // When travelling north, pick a random cell to carve north from out of
+        // When travelling north, pick a random cell to carve north from one of
         // the stored 'run' cells.
         if direction == Dir2::NORTH {
             let run_index = fastrand::usize(0..run.len());
             let run_cell = run[run_index];
-
-            let neighbour = grid_map.neighbour_from_cell_pos(run_cell, direction);
-            if neighbour.is_some() {
-                let wall = grid_map.wall_from_cell_pos(run_cell, direction);
-                if let Some(wall) = wall {
-                    removed_walls.insert(wall);
-                }
-            }
+            wall = grid_map.inner_wall_from_cell_pos(run_cell, direction);
             run.clear();
+        }
+
+        // Remove the chosen wall.
+        if let Some(wall) = wall {
+            removed_walls.insert(wall);
         }
     });
 
